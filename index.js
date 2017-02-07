79461170ce955d4f4614dd27d5fd1d030d2e0027
sridevi peddi <sridevip9@yahoo.com>
2017-02-07 13:26:36 -0600

    First Express app

---
+++ index.js
@@ -0,0 +1,27 @@
+/*Exercise 1
+
+Create a new project directory for tonight's exercises.
+Make this directory a git repository.
+Initialize this directory as an NPM module
+Install Express
+Create a new javascript file in this directory.
+Create an express application
+Test with curl, Postman and the browser
+Save and commit any changes to the git repository
+Push these changes to GitHub
+*/
+
+var express = require ('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Creating  a new express appication');
+});
+
+app.get('/about', function(req, res){
+  res.send('Creating  a new path about express appication');
+});
+
+app.listen(3000, function(){
+  console.log("server listening port 3000...")
+})
